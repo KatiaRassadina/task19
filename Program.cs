4e371003ba7117b7001273734fdb2ccaa62f807c
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-10-23 23:04:42 +0300

    not finished

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа принимает на вход 5-значное число и проверяет является ли оно палиндромом
+Console.WriteLine("Input five-digit number");
+Convert.ToInt32(Console.ReadLine());
+int [] array = new int[5];
+int index = 0;
+int Length = 5;
+if(Length > 5 && Length < 5)
+{Console.WriteLine("Input five-digit number");};
+if(array[0] == array[4] ||
+   array[1] == array[3]);
+{
+    Console.WriteLine("it is a palindrome");
+    };
+
+
